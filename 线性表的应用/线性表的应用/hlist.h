ba5d8eef720536c0b108207b1a7854e49c682edb
Invincible-Shady <zk1263307973@outlook.com>
2017-10-03 22:16:07 +0800

    a little change

--- 线性表的应用/线性表的应用/hlist.h
+++ 线性表的应用/线性表的应用/hlist.h
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #pragma once
 #include<malloc.h>
 
@@ -6,5 +7,56 @@
 typedef struct Node1 {
 	ElemType data[MaxCol];
 	struct Node1 *next;
-};
+}DList;
 
+typedef struct Node2 {
+	int Row, Col;
+	DList *next;
+}HList;
+
+void CreatTable(HList *&h) {//采用互交式方式建立单链表的算法
+	int i, j;
+	DList *r, *s;
+	h = (HList *)malloc(sizeof(HList));
+	printf("表的行数，列数：");
+	scanf("%d%d", &h->Row, &h->Col);
+	for (i = 0; i < h->Row; i++) {
+		printf("第%d行：", i + 1);
+		s = (DList *)malloc(sizeof(DList));
+		for (j = 0; j < h->Col; j++) {
+			scanf("%d", &s->data[j]);
+		}
+		if (h->next == NULL) {
+			h->next = s;
+		}
+		else {
+			r->next = s;
+		}
+		r = s;
+	}
+	r->next = NULL;
+}
+
+void DestoryTable(HList *&h) {//销毁单链表的算法
+	DList *pre = h->next, *p = pre->next;
+	while (p != NULL) {
+		free(pre);
+		pre = p;
+		p = p->next;
+	}
+}
+
+void DispTable(HList *h) {//输出单链表的算法
+	int j;
+	DList *p=h->next;
+	while (p!=NULL)
+	{
+		for (j = 0; j < h->Col; j++) {
+			printf("%4d", p->data[j]);
+		}
+		printf("\n");
+		p = p->next;
+	}
+}
+
+void 
