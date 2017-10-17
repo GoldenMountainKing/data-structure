15be4cd69c5aabe7ff501332ac991281739ffb6a
Invincible-Shady <1263307973@qq.com>
2017-10-17 22:50:53 +0800

    在题目上疏忽了一个递增排序，补一下

--- p76 实验8 求集合（用单链表表示）的并，交和差运算/exp2-8/exp2-8/linklist.h
+++ p76 实验8 求集合（用单链表表示）的并，交和差运算/exp2-8/exp2-8/linklist.h
@@ -1,28 +1,28 @@
-//���������������㷨
+﻿//单链表基本运算算法
 #include <stdio.h>
 #include <malloc.h>
 typedef int ElemType;
 typedef struct LNode  
 {
 	ElemType data;
-	struct LNode *next;		//ָ���̽��
-} LinkNode;					//�����������������
+	struct LNode *next;		//指向后继结点
+} LinkNode;					//声明单链表结点类型
 
 void CreateListR(LinkNode *&L,ElemType a[],int n)
-//β�巨����������
+//尾插法建立单链表
 {
 	LinkNode *s,*r;
-	L=(LinkNode *)malloc(sizeof(LinkNode));  	//����ͷ���
+	L=(LinkNode *)malloc(sizeof(LinkNode));  	//创建头结点
 	L->next=NULL;
-	r=L;					//rʼ��ָ���ն˽��,��ʼʱָ��ͷ���
+	r=L;					//r始终指向终端结点,开始时指向头结点
 	for (int i=0;i<n;i++)
 	{	
-		s=(LinkNode *)malloc(sizeof(LinkNode));//�����½��s
+		s=(LinkNode *)malloc(sizeof(LinkNode));//创建新结点s
 		s->data=a[i];
-		r->next=s;			//�����s������r֮��
+		r->next=s;			//将结点s插入结点r之后
 		r=s;
 	}
-	r->next=NULL;			//�ն˽��next����ΪNULL
+	r->next=NULL;			//终端结点next域置为NULL
 }
 
 void DestroyList(LinkNode *&L)
@@ -33,7 +33,7 @@ void DestroyList(LinkNode *&L)
 		pre=p;
 		p=pre->next;
 	}
-	free(pre);	//��ʱpΪNULL,preָ��β���,�ͷ���
+	free(pre);	//此时p为NULL,pre指向尾结点,释放它
 }
 
 
@@ -48,7 +48,25 @@ void DispList(LinkNode *L)
 }
 
 
-//���õ�������������ʱ�Ķ�·�鲢�㷨(�󲢼���
+void UnionList0(LinkNode *&L) {//递增排序
+	LinkNode *p, *pre, *q;
+	p = L->next->next;
+	L->next->next = NULL;
+	while (p!=NULL)
+	{
+		q = p->next;
+		pre = L;
+		while (pre->next != NULL && pre->next->data < p->data)
+		{
+			pre = pre->next;
+		}
+		p->next = pre->next;
+		pre->next = p;
+		p = q;
+	}
+}
+
+//采用单链表存放有序表时的二路归并算法(求并集）
 void UnionList1(LinkNode *LA,LinkNode *LB,LinkNode *&LC){
 	LinkNode *pa=LA->next,*pb=LB->next,*r,*s;
 	LC=(LinkNode *)malloc(sizeof(LinkNode));
@@ -95,38 +113,38 @@ void UnionList1(LinkNode *LA,LinkNode *LB,LinkNode *&LC){
 	r->next=NULL;
 }
 
-//���õ�������������ʱ�Ķ�·�鲢�㷨(����
+//采用单链表存放有序表时的二路归并算法(求差集）
 void UnionList2(LinkNode *LA,LinkNode *LB,LinkNode *&LD){
 	LinkNode *pa=LA->next,*pb=LB->next,*r,*s;
-	LD=(LinkNode *)malloc(sizeof(LinkNode));//�����ڴ�ռ�
-	r=LD;//rָ��LDͷ��ַ
-	while(pa->next!=NULL){//pa��û��ѭ����β��
+	LD=(LinkNode *)malloc(sizeof(LinkNode));//分配内存空间
+	r=LD;//r指向LD头地址
+	while(pa->next!=NULL){//pa还没有循环到尾部
 
-		pb=LB->next;//pbָ��LB�ĵ�һ��Ԫ��
+		pb=LB->next;//pb指向LB的第一个元素
 
-		while(pa->data!=pb->data){//��pa������pbʱ
+		while(pa->data!=pb->data){//当pa不等于pb时
 			if(pb->next!=NULL){
-				pb=pb->next;//pb���ϵ����ѭ����ֱ��pb��β��
+				pb=pb->next;//pb不断的向后循环，直到pb的尾部
 			}
-			else{//pa����pbʱ
-				s=(LinkNode *)malloc(sizeof(LinkNode));//Ϊ��Ҫ�洢���ڴ�����ڴ�ռ�
-				s->data=pa->data;//��s�д��pa��ֵ
-				r->next=s;r=s;//��s����ld�ĺ���
-				break;//����ѭ��
+			else{//pa等于pb时
+				s=(LinkNode *)malloc(sizeof(LinkNode));//为将要存储的内存分配内存空间
+				s->data=pa->data;//将s中存放pa的值
+				r->next=s;r=s;//将s连在ld的后面
+				break;//跳出循环
 			}
 		}
 
 		pa=pa->next;
 	}
-	r->next=NULL;//�������µ�������ĩβָ���ָ��
+	r->next=NULL;//最后的在新的链表的末尾指向空指针
 }
 
-void UnionList3(LinkNode *LA, LinkNode *LB, LinkNode *&LE) {//�󽻼����㷨
-	LinkNode *pa = LA->next, *pb = LB->next, *r, *s;//s��һ����ʱ�洢���õ������Ľ��
+void UnionList3(LinkNode *LA, LinkNode *LB, LinkNode *&LE) {//求交集的算法
+	LinkNode *pa = LA->next, *pb = LB->next, *r, *s;//s是一个临时存储所得到变量的结点
 	LE = (LinkNode *)malloc(sizeof(LinkNode));
 	r = LE;
 
-	while (pa != NULL||pb != NULL) {//��
+	while (pa != NULL||pb != NULL) {//或
 		if (pb == NULL) {
 			pa = pa->next;
 			pb = LB->next;
--- p76 实验8 求集合（用单链表表示）的并，交和差运算/exp2-8/exp2-8/main.cpp
+++ p76 实验8 求集合（用单链表表示）的并，交和差运算/exp2-8/exp2-8/main.cpp
@@ -12,12 +12,16 @@ void main(){
 	printf("建立单链表：L1，L2,L3,L4,L5\n");//L3是并集，L4是差集，L5是交集
 	LinkNode *L1,*L2,*L3,*L4,*L5;
 	printf("建立俩个集合：ElemType a[],ElemType b[]\n");
-	ElemType a[]={1,3,4,5,7,8};
-	ElemType b[]={1,2,5,6,8};
+	ElemType a[]={1,4,3,5,7,8};
+	ElemType b[]={2,1,6,5,8};
 	printf("初始化单链表L1,L2，存入a，b的元素\n");
 	CreateListR(L1, a,6);
 	CreateListR(L2, b,5);
 	DispList(L1);DispList(L2);
+	printf("按递增排序\n");
+	UnionList0(L1); UnionList0(L2);
+	printf("按递增排序后的链表为：\n");
+	DispList(L1); DispList(L2);
 	printf("采用二路归并求并");
 	UnionList1(L1,L2,L3);
 	printf("输出合并后的表");
